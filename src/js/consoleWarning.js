3024bda73ff384489f92e2b4d524211b7d602304
rokobuljan <roko.cb@gmail.com>
2026-07-16 14:28:22 +0200

    add: console phishing warning

---
+++ src/js/consoleWarning.js
@@ -0,0 +1,29 @@
+// CONSOLE WARNING - ANTI-PHISHING
+const lsName = "xode-isDeveloper";
+const showWarning = function () {
+    console.log(`%c⚠️ SECURITY WARNING`, `
+    font-size: 2rem;
+    font-weight: bold;
+    color: #f00;
+    text-shadow: 0 0 20px rgba(255,0,0,0.3);`);
+    console.log(`%cSTOP WHAT YOU'RE DOING!
+Pasting anything into the console could compromise you.
+XODE (this app) will NEVER ask you to paste code here. This is a phishing scam tactic.
+Only use the console if you're a developer and understand what you're doing.
+If someone directed you here, they're trying to steal your data.
+
+To hide this warning, run: i_am_a_xode_developer()`, `
+    border-left: 0.5rem solid #f00;
+    font-size: 1rem;
+    padding: 0rem 1rem;`);
+};
+
+globalThis.i_am_a_xode_developer = function () {
+    localStorage.setItem(lsName, true);
+    console.clear();
+};
+
+if (!localStorage.getItem(lsName)) {
+    showWarning();
+}
+
